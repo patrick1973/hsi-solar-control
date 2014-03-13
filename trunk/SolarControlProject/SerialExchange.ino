977411ff2228c79fb5973e85da354b64557df783
misteriepie@gmail.com <misteriepie@gmail.com@11e8c90b-6824-644d-b166-0251b339b419>
2014-03-13 23:06:51 +0000

    state machine toegevoegd

--- trunk/SolarControlProject/SerialExchange.ino
+++ trunk/SolarControlProject/SerialExchange.ino
@@ -12,4 +12,24 @@ void showLdrValuesSerial()
   Serial.println("end");
 }
 
+String getStringBetween(String strSource, char charBegin, char charEnd)
+{
+  int startIndex, stopIndex;
+  startIndex = strSource.indexOf(charBegin)+1;
+  stopIndex =  strSource.indexOf(charEnd);
+  return strSource.substring(startIndex,stopIndex);
+}
+
+String getSerialCommand()
+{
+  String inputString = "";
+  while(Serial.available())
+  {
+    char inChar = (char)Serial.read(); 
+    
+    inputString += inChar;
+  } 
+  return getStringBetween(inputString, '$', '#');
+}
+
 
