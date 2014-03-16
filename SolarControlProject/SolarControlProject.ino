07354f1d8186cbb5dc359c0df9b98b34cfddb7a2
misteriepie@gmail.com <misteriepie@gmail.com@11e8c90b-6824-644d-b166-0251b339b419>
2014-03-16 22:32:08 +0000

    I2c ADC toegevoegd. NTC toegevoegd routine gemaakt voor het uitlezen van de ADC

--- SolarControlProject/SolarControlProject.ino
+++ SolarControlProject/SolarControlProject.ino
@@ -1,11 +1,16 @@
 #include <Servo.h>
 #include <LiquidCrystal.h>
+#include <Wire.h>
 #include "Init.h"
 
 
 int teller = 0;
-float temp = 0.0;
+float temp1 = 0.0;
+float temp2 = 0.0;
+float treshhold=0.0;
 int SystemState;
+
+
 void setup()
 { 
   setupIO(); 
@@ -13,16 +18,31 @@ void setup()
 
 void loop()
 {
+  readADCvalues(&adcValues0,&adcValues1,&adcValues2,&adcValues3); // update every cycle the ADC converter. Can be a better wat to do this, but for now....
   SystemState = getSerialSystemState();
   showLdrValuesSerial();
+  
   teller++;
-  if (teller >= 600)
+  if (teller >= 60)
   {
-    temp = ntcTemperature(readNtcValue());
+    temp1 = ntcTemperature(readNtcValueOutside());
+    temp2 = ntcTemperature(readNtcValuePanel());
+    treshhold = ntcTemperature(readPotValueEODS());
+    
+    lcd.setCursor(0,1);
+    lcd.print(temp1);
+    lcd.print("#");
+    lcd.print(temp2);
+    lcd.print("#");
+    lcd.print(treshhold);
+    lcd.print("#");
+    
+    
     teller =0;
-  }
+  }  
   delay(50);
 }
+
 
 
 
